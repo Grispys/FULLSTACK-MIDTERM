2317767af499ff47cf5cfe23be0aeee548554067
Grispys <grispanimation@gmail.com>
2024-10-27 15:29:19 -0230

    completely revamped how randomMenuItem test works. now it checks that a specific item with its correct values are made, not just thattwo strings and a number are produced. yahoo!

--- __tests__/utils/restaurantUtils.test.js
+++ __tests__/utils/restaurantUtils.test.js
@@ -2,30 +2,38 @@ const { Cuisines, Dishes } = require("../../utils/data");
 const { generateRandomMenuItem, generateMenu, selectRandomCuisine } = require("../../utils/restaurantUtils");
 
 describe('Restaurant Functions', () => {
-    test('Tests that generateRandomMenuItem correctly makes an item', () => {
-      let randomCuisine = selectRandomCuisine()
-      let randomItem = generateRandomMenuItem(randomCuisine)
-      let expectedFormat ={
-        dish: {
-          name: expect.any(String),  // i also tried forever to get this to look for a specific item with its name and matchin description and price and i just couldnt
-                                     // do it. i'm sorry. but hey, atleast it tests that what it generates is an object that matches this structure.
-          description: expect.any(String),
-          price: expect.any(Number)
-        },
-        // right quick, i'm going to be honest, I hate regex. I googled how to make a regex
-        // that accepts a specific string or an empty one. I know, i should know regex off
-        // the top of my head like every good programmer (sarcasm) but i caved into google.
-        // forgive me
-        special: expect.stringMatching(/^(Daily Special!!!)?$/)
-        
-      }
-      expect(randomItem).toMatchObject(expectedFormat)
-    });
+  test('Tests that generateRandomMenuItem correctly makes an item', () => {
+    let randomCuisine = selectRandomCuisine();
+    let randomItem = generateRandomMenuItem(randomCuisine);
+    let dishesArray = Dishes[randomCuisine];
+
+    // i completely rearranged how a correct dish is generated and tested because the last one only checked that a string and number were made, not
+    // that a correct item from the available menu was actuall there. now, it checks that its name, description and price fits one of the items in the availabler menu
+    let isValidDish = dishesArray.some(dish => 
+      dish.name === randomItem.dish.name &&
+      dish.description === randomItem.dish.description &&
+      dish.price === randomItem.dish.price
+    );
+
+    expect(isValidDish).toBe(true); 
+
+    // this chjecks that special is either empty or is a daily special. i decided against using regex because i forgot that special returns a string
+    let isSpecialValid = randomItem.special === "" || randomItem.special === "Daily Special!!!"; 
+    expect(isSpecialValid).toBe(true)
+  });
+
+
+
+
 
     describe('generateMenu', () => {
       // Test implementations go here
     });
 
+
+
+
+    
     test('Tests that selectRandomCuisine correctly picks 1 of 5 random cuisines from the Cuisines array', () => {
       let randomCuisine = selectRandomCuisine();
       let availableResults = ['italian','indian','chinese','vegan','mexican',]
